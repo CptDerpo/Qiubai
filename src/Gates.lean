694c3f4a8f4fbca9ab441286b38deb15d25aca7d
CptDerpo <zahir.bingen@gmail.com>
2023-02-23 03:23:48 +0100

    Added definitions for logic gates: not, and, nand, or, nor, xor.

---
+++ src/Gates.lean
@@ -0,0 +1,30 @@
+def AND : list bool → bool
+  | [] := true
+  | (h::t) := h ∧ AND t
+
+def OR : list bool -> bool
+  | [] := false
+  | (h::t) := h ∨ OR t
+
+def XOR : list bool → bool
+  | [] := false
+  | [b] := b
+  | (h1::h2::t) := (h1 ≠ h2) ∧ XOR (h2::t)
+
+def not_ (a : bool) (out : bool) : bool :=
+  out = ¬a
+
+def and_n (a : list bool) (out : bool) : bool :=
+  out = AND a
+
+def nand_n (a : list bool) (out : bool) : bool :=
+  out = ¬AND a
+
+def or_n (a : list bool) (out : bool) : bool :=
+  out = OR a
+
+def nor_n (a : list bool) (out : bool) : bool :=
+  out = ¬OR a
+
+def xor_n (a : list bool) (out : bool) : bool :=
+  out = XOR a
