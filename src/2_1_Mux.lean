81898a43ee777ddc1ed13062919403891563b4cb
CptDerpo <zahir.bingen@gmail.com>
2023-02-23 03:30:12 +0100

    Added correctness proof of 2to1 Multiplexer.

--- src/2_1_Mux.lean
+++ src/2_1_Mux.lean
@@ -1,4 +1,4 @@
-import Gates
+import Gates tactic
 
 def mux_imp (in1 in2 sel out : bool) : Prop :=
 	∃ (p q r : bool), (nand_n [in1, p] q) ∧ (nand_n [sel, in2] r) ∧ 
@@ -7,3 +7,27 @@ def mux_imp (in1 in2 sel out : bool) : Prop :=
 def mux_spec (in1 in2 sel out : bool) : Prop :=
 	if sel then (out = in2) else (out = in1) 
 
+theorem mux_correct : ∀ in1 in2 sel out, mux_imp in1 in2 sel out ↔ mux_spec in1 in2 sel out :=
+begin
+  intros in1 in2 sel out,
+  unfold mux_imp mux_spec nand_n AND not_,
+  split,
+  {
+    cases sel; simp,
+    { --sel FF
+      cases in1; simp,
+    },
+    { --sel TT
+      cases in2; simp,
+    }
+  },
+  {
+    cases sel; simp,
+    { -- sel FF
+      cases in1; simp,
+    },
+    { -- sel TT
+      cases in2; simp,
+    }
+  }
+end
