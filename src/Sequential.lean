5b463c4f848f55a6fe209216126902bfa9abb88e
CptDerpo <zahir.bingen@gmail.com>
2023-04-29 21:36:51 +0200

    Cleanup of unnecessary functions

--- src/Sequential.lean
+++ src/Sequential.lean
@@ -25,9 +25,4 @@ def n_mem_spec {n : ℕ} (D : sig_n n) (S : signal) (M: sig_n n): Prop :=
   ∀ t : ℕ,
     (S t = tt → M (nat.succ t) = D t) ∧  --frame 1 D S M+1
     (S t = ff → M (nat.succ t) = M t) --frame 2 S M M+1
-
-def ncounter_spec {n : ℕ} (out : sig_n n) (res : signal) : Prop :=
-  ∀ t : ℕ, 
-  if res t = tt 
-  then bool_arr_to_nat(out (t+1)) = 0
-  else bool_arr_to_nat(out (t+1)) = (bool_arr_to_nat(out t) + 1) % 2^(n)
+    
