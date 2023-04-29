8dde6b4a4fabd6855ffdcc36eccb8669f7a0fa95
CptDerpo <zahir.bingen@gmail.com>
2023-04-29 21:35:58 +0200

    Fixed multiplexer and added mux_n_imp correctly

--- src/Combinational.lean
+++ src/Combinational.lean
@@ -1,19 +1,19 @@
 import Gates tactic Conversions data.vector data.real.basic Sequential
 
 --2to1 mux with 1 bit input
-def mux_imp (in1 in2 sel out : bool) : Prop :=
-	∃ (p q r : bool), (nand_n [in1, p] q) ∧ (nand_n [sel, in2] r) ∧ 
-		(nand_n [q, r] out) ∧ (not_ sel p)
+def mux_imp (in1 in2 sel : bool) : bool :=
+	∃ (p q r out : bool), (nand_n [in1, p] q) ∧ (nand_n [sel, in2] r) ∧ 
+		(nand_n [q, r] out) ∧ (not_ sel p) ∧ out
 
 def mux_spec (in1 in2 sel out : bool) : Prop :=
 	if sel then (out = in2) else (out = in1) 
 
 --2to1 mux with n-bit inputs
-def mux_n_spec {n : ℕ} (in1 in2 : array n bool) (sel : bool) (out : array n bool) : Prop :=
-	∀ i : fin n, mux_spec (in1.read i) (in2.read i) sel (out.read i) 
+def mux_n_imp {n : ℕ} (in1 in2 : array n bool) (sel : bool) : array n bool :=
+  (zip_array in1 in2).map (λ x, mux_imp x.fst x.snd sel)
 
-def mux_n_imp {n : ℕ} (in1 in2 : array n bool) (sel : bool) (out : array n bool) : Prop :=
-	∀ i : fin n, mux_imp (in1.read i) (in2.read i) sel (out.read i)
+def mux_n_spec {n : ℕ} (in1 in2 : array n bool) (sel : bool) (out : array n bool) : Prop :=
+	if sel then (out = in2) else (out = in1)
 
 --2to4 decoder
 def dec_imp (in0 in1 out0 out1 out2 out3 : bool) : Prop :=
@@ -26,7 +26,6 @@ def dec_spec (in0 in1 out0 out1 out2 out3 : bool) : Prop :=
 	else if in1 ∧ ¬in0 then (out3 = ff ∧ out2 = tt ∧ out1 = ff ∧ out0 = ff)
 	else (out3 = tt ∧ out2 = ff ∧ out1 = ff ∧ out0 = ff)
 
-
 --full-adder
 def full_adder_imp (A B Cin : bool) (Sum Cout : bool) : Prop :=
 	∃ (p q r : bool), (xor_n [A, B] p) ∧ (xor_n [p, Cin] Sum) ∧ (and_n [Cin, p] q) ∧ (and_n [A, B] r)
@@ -35,3 +34,11 @@ def full_adder_imp (A B Cin : bool) (Sum Cout : bool) : Prop :=
 def full_adder_spec (A B Cin : bool) (Sum Cout : bool) : Prop :=
 	Sum = nat_to_bool ((bool_to_nat A + bool_to_nat B + bool_to_nat Cin) % 2) ∧ 
 	Cout = ((bool_to_nat A + bool_to_nat B + bool_to_nat Cin) ≥ 2)
+
+--adder with n-bit inputs
+def adder_n_imp {n : ℕ} (A B : array n bool) (Sum : array n bool) (Cout : bool) : Prop :=
+	 
+	
+def adder_n_spec {n : ℕ} (A B : array n bool) (Sum : array n bool) (Cout : bool) : Prop :=
+
+
