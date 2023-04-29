715532638e7572343edbeb939323ddeb0bfc525e
CptDerpo <zahir.bingen@gmail.com>
2023-04-29 21:31:38 +0200

    Added Zip function for arrays, cleanup unnecessary functions

--- src/Conversions.lean
+++ src/Conversions.lean
@@ -13,26 +13,10 @@ def bool_list_to_nat : list bool → ℕ
   | (b :: bools) := if b then 2^(list.length bools) + bool_list_to_nat bools
                     else bool_list_to_nat bools
 
-def bool_vec_to_nat {n : ℕ} (v : vector bool n) : ℕ :=
-  bool_list_to_nat (list.of_fn v.nth)
-
 def bool_arr_to_nat {n : ℕ} (v : array n bool) : ℕ :=
   bool_list_to_nat (array.to_list v)
 
-def get_nth_bool : list bool -> ℕ -> bool
-  | [] n := ff
-  | (h::t) 0 := h
-  | (h::t) (n+1) := get_nth_bool t n
+def zip_array {n : ℕ} {α β : Type} (a : array n α) (b : array n β) : array n (α × β) :=
+  ⟨λ i, (a.read i, b.read i)⟩
 
-def get_first_half {α : Type*} : list α → list α --Type* is Type 0, smallest universe, non dependent types
-  | [] := []
-  | [a] := [a]
-  | lst := list.take (lst.length / 2) lst
-
-def get_second_half {α : Type*} : list α → list α
-  | [] := []
-  | [a] := [a]
-  | lst := list.drop (lst.length / 2) lst
-
-#eval get_first_half [tt, tt, ff, tt, ff, tt, ff]
-#eval get_second_half [tt, tt, ff, tt, ff, tt, ff]
+#eval zip_array ([tt, tt, tt].to_array) ([ff, ff, ff].to_array)
