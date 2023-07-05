40ce2a5e8dbceacb77f6f9e4883492175faf9325
CptDerpo <zahir.bingen@gmail.com>
2023-07-05 05:18:43 +0200

    Restructured sequential code

--- src/Sequential.lean
+++ src/Sequential.lean
@@ -1,4 +1,4 @@
-import Gates tactic Conversions
+import Gates tactic Conversions Combinational
 
 def stream (α : Type) := ℕ → α
 
@@ -6,23 +6,29 @@ def signal := ℕ → bool
 
 def sig_n (n : ℕ) := ℕ → array n bool
 
+def sig_n_zero (n : ℕ) : sig_n n := λ t, mk_array n ff
+
+def sig_tt : signal := λ t, tt
+
 def mem_imp (I: bool) (D S: signal) : signal
 | nat.zero := I
 | (nat.succ y) := if S y = tt then D y 
                   else mem_imp y
 
-def mem_spec (D S M: signal): Prop :=
+
+def mem_spec {α : Type} (D : ℕ → α) (S : signal) (M : ℕ → α) : Prop :=
   ∀ t : ℕ,
-    (S t = tt → M (nat.succ t) = D t) ∧ --frame 1 D S M+1
-    (S t = ff → M (nat.succ t) = M t) --frame 2 S M M+1
+    (S t = tt → M (nat.succ t) = D t) ∧  --frame 1 D S M+1
+    (S t = ff → M (nat.succ t) = M t) --frame 2 S M M+1  
+
 
 def n_mem_imp {n : ℕ} (I : array n bool) (D : sig_n n) (S : signal) : sig_n n
   | nat.zero := I
   | (nat.succ y) := if S y = tt then D y
                     else n_mem_imp y
 
-def n_mem_spec {n : ℕ} (D : sig_n n) (S : signal) (M: sig_n n): Prop :=
+def n_mem_spec {n : ℕ} (D : sig_n n) (S : signal) (M : sig_n n): Prop :=
   ∀ t : ℕ,
     (S t = tt → M (nat.succ t) = D t) ∧  --frame 1 D S M+1
     (S t = ff → M (nat.succ t) = M t) --frame 2 S M M+1
-    
+
