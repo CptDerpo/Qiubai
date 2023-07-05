3158c55f1177f42a76c276801c6da656b28393bb
CptDerpo <zahir.bingen@gmail.com>
2023-07-05 05:19:21 +0200

    Changed left shift to use logic gates in implementation

--- src/Combinational.lean
+++ src/Combinational.lean
@@ -1,44 +1,56 @@
-import Gates tactic Conversions data.vector data.real.basic Sequential
+import Gates tactic Conversions data.vector data.real.basic
 
 --2to1 mux with 1 bit input
 def mux_imp (in1 in2 sel : bool) : bool :=
-	∃ (p q r out : bool), (nand_n [in1, p] q) ∧ (nand_n [sel, in2] r) ∧ 
-		(nand_n [q, r] out) ∧ (not_ sel p) ∧ out
+	let p := NOT sel,
+			q := AND [in1, p],
+			r := AND [in2, sel] in
+	(OR [q, r])
 
-def mux_spec (in1 in2 sel out : bool) : Prop :=
-	if sel then (out = in2) else (out = in1) 
+def mux_spec {α : Type} (IN0 IN1 : α) (SEL : bool) (OUT : α) : Prop :=
+	if SEL then (OUT = IN1) else (OUT = IN0) 
 
 --2to1 mux with n-bit inputs
 def mux_n_imp {n : ℕ} (in1 in2 : array n bool) (sel : bool) : array n bool :=
   (zip_array in1 in2).map (λ x, mux_imp x.fst x.snd sel)
 
-def mux_n_spec {n : ℕ} (in1 in2 : array n bool) (sel : bool) (out : array n bool) : Prop :=
-	if sel then (out = in2) else (out = in1)
+def full_adder_imp (A B Cin : bool) : (bool × bool) :=
+	let p := XOR [A, B],
+			q := AND [Cin, p],
+			r := AND [A, B] in
+	(XOR [p, Cin], OR [q, r])
 
---2to4 decoder
-def dec_imp (in0 in1 out0 out1 out2 out3 : bool) : Prop :=
-	∃ (p q : bool), (not_ in0 p) ∧ (not_ in1 q) ∧ (and_n [p, q] out0) ∧ 
-		(and_n [in0, q] out1) ∧ (and_n [p, in1] out2) ∧ (and_n [in0, in1] out3)
+def full_adder_spec (A B Cin Sum Cout : bool) : Prop :=
+	Sum = nat_to_bool ((bool_to_nat A + bool_to_nat B + bool_to_nat Cin) % 2) ∧ --sum
+	Cout = ((bool_to_nat A + bool_to_nat B + bool_to_nat Cin) ≥ 2) --carry out
 
-def dec_spec (in0 in1 out0 out1 out2 out3 : bool) : Prop :=
-	if ¬in1 ∧ ¬in0 then (out3 = ff ∧ out2 = ff ∧ out1 = ff ∧ out0 = tt)
-	else if ¬in1 ∧ in0 then (out3 = ff ∧ out2 = ff ∧ out1 = tt ∧ out0 = ff)
-	else if in1 ∧ ¬in0 then (out3 = ff ∧ out2 = tt ∧ out1 = ff ∧ out0 = ff)
-	else (out3 = tt ∧ out2 = ff ∧ out1 = ff ∧ out0 = ff)
+def full_n_adder_imp {n : ℕ} (A B : array n bool) (Cin : bool) : (array n bool × bool) :=
+	fold_array (full_adder_imp) (zip_array A B) (mk_array n ff, Cin)
 
---full-adder
-def full_adder_imp (A B Cin : bool) (Sum Cout : bool) : Prop :=
-	∃ (p q r : bool), (xor_n [A, B] p) ∧ (xor_n [p, Cin] Sum) ∧ (and_n [Cin, p] q) ∧ (and_n [A, B] r)
-		∧ (or_n [q, r] Cout)
 
-def full_adder_spec (A B Cin : bool) (Sum Cout : bool) : Prop :=
-	Sum = nat_to_bool ((bool_to_nat A + bool_to_nat B + bool_to_nat Cin) % 2) ∧ 
-	Cout = ((bool_to_nat A + bool_to_nat B + bool_to_nat Cin) ≥ 2)
+def full_n_adder_spec {n : ℕ} (A B : array n bool) (Cin : bool) (Sum : array n bool) (Cout : bool) : Prop :=
+	bool_arr_to_nat Sum = (bool_arr_to_nat A + bool_arr_to_nat B + bool_to_nat Cin) % 2^n ∧
+	Cout = ((bool_arr_to_nat A + bool_arr_to_nat B + bool_to_nat Cin) ≥ 2^n)
 
---adder with n-bit inputs
-def adder_n_imp {n : ℕ} (A B : array n bool) (Sum : array n bool) (Cout : bool) : Prop :=
-	 
+def lsh_elementary (In I : bool) : (bool × bool) :=
+	let p := AND[I, tt],
+			q := AND[In, ff] in
+	(OR[p, q], In)
+
+def lsh_imp {n : ℕ} (A : array n bool) : array n bool :=
+	fold_lsh (lsh_elementary) A (mk_array n ff, ff)
+
+def lsh_spec {n : ℕ} (A out : array n bool) : Prop :=
+  bool_arr_to_nat out = (bool_arr_to_nat A * 2) % (2^n)
+
+def ALU_lsh_imp {n : ℕ} (A : array n bool) : array n bool :=
+  ⟨λ i, if h : i.val + 1 < n then A.read ⟨i.val + 1, h⟩ else ff⟩
+
+--right shift
+def ALU_rsh_imp {n : ℕ} (A : array n bool) : array n bool :=
+	⟨λ i, if h : i.val > 0 ∧ i.val - 1 < n then A.read ⟨i.val - 1, h.right⟩ else ff⟩
+
+def ALU_rsh_spec {n : ℕ} (A out : array n bool) : Prop :=
+  bool_arr_to_nat out = (bool_arr_to_nat A) / 2
 	
-def adder_n_spec {n : ℕ} (A B : array n bool) (Sum : array n bool) (Cout : bool) : Prop :=
-
 
